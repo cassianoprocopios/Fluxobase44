f9e4e3427d9377943bbcd8df2216b834bcc9711a
Cassiano Procopio <cassianoprocopios@gmail.com>
2026-03-22 23:48:22 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import { ConciliationProvider } from '@/lib/ConciliationContext';
 
 import AppLayout from '@/components/layout/AppLayout';
 import Dashboard from '@/pages/Dashboard';

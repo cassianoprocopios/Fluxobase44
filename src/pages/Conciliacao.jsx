23264f88bf01fb188d7f4f0633feef3963184d0b
Cassiano Procopio <cassianoprocopios@gmail.com>
2026-03-23 00:32:35 +0000

    File changes

--- src/pages/Conciliacao.jsx
+++ src/pages/Conciliacao.jsx
@@ -17,7 +17,6 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { ArrowLeft, CheckCheck, Download, Square, CheckSquare, MinusSquare, Trash2, Plus } from "lucide-react";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "@/components/ui/use-toast";
 

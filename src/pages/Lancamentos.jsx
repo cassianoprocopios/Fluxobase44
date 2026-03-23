171fa73f77113968abdfe7797ea9139391e69a36
Cassiano Procopio <cassianoprocopios@gmail.com>
2026-03-23 00:27:28 +0000

    File changes

--- src/pages/Lancamentos.jsx
+++ src/pages/Lancamentos.jsx
@@ -125,6 +125,12 @@ export default function Lancamentos() {
           </p>
         </div>
         <div className="flex gap-2">
+          <Link to="/conciliacao">
+            <Button variant="outline" size="sm">
+              <ArrowLeftRight className="w-4 h-4 mr-2" />
+              Conciliação
+            </Button>
+          </Link>
           <Link to="/importar">
             <Button variant="outline" size="sm">
               <Upload className="w-4 h-4 mr-2" />
